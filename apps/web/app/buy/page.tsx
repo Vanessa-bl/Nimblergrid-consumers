2a21ad348c98f25415637d902afe17a1051f8943
Vanessa-bl <barreralapentanv@gmail.com>
2026-09-08 14:42:38 +0200

    004 add routes

---
+++ apps/web/app/buy/page.tsx
@@ -0,0 +1,7 @@
+export default function BuyPage() {
+  return (
+    <div className="flex min-h-[50vh] items-center justify-center">
+      <h1 className="text-xl font-medium text-zinc-600">Próximamente...</h1>
+    </div>
+  );
+}
---
+++ apps/web/app/real-state-agencies/page.tsx
@@ -0,0 +1,7 @@
+export default function RealStateAgenciesPage() {
+  return (
+    <div className="flex min-h-[50vh] items-center justify-center">
+      <h1 className="text-xl font-medium text-zinc-600">Próximamente...</h1>
+    </div>
+  );
+}
